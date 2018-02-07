4d43772564b416270900b207905a8996c12d46fd
Tao <Tao@Tao-VAIO>
2018-02-07 09:13:27 -0800

    Unfinished Lab03c data

---
+++ APCSA/APCSA/unit3/Lab03c.java
@@ -0,0 +1,17 @@
+package unit3;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Lab03c
+{
+	public static void main( String[] args )
+   {
+   	//add test cases
+   	
+   	
+		
+		
+	}
+}
---
+++ APCSA/APCSA/unit3/Quadratic.java
@@ -0,0 +1,44 @@
+package unit3;
+
+import java.util.Scanner;
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class Quadratic
+{
+	private int a, b, c;
+	private double rootOne;
+	private double rootTwo;
+
+	public Quadratic()
+	{
+
+
+	}
+
+	public Quadratic(int quadA, int quadB, int quadC)
+	{
+		float qA = quadA;
+		float qB = quadB;
+		float qC = quadC;
+	}
+
+	public void setEquation(int quadA, int quadB, int quadC)
+	{
+
+
+ 	}
+
+	public void calcRoots( )
+	{
+		//rootone = -qB + //finish this statement
+
+
+	}
+
+	public void print( )
+	{
+
+
+	}
+}
